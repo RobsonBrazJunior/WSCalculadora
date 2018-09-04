4648fd284a8a2515b8f5910963d530ad519dcc31
Junior Braz <robsonbrazjunior@gmail.com>
2018-09-03 21:45:48 -0400

    Atualizado modelo de negocio

--- src/java/servico/ServicoCalculadora.java
+++ src/java/servico/ServicoCalculadora.java
@@ -2,31 +2,28 @@ package servico;
 
 import javax.jws.WebService;
 import javax.jws.WebMethod;
+import negocio.Calculadora;
 
 @WebService(serviceName = "ServicoCalculadora")
 public class ServicoCalculadora {
 
     @WebMethod(operationName = "somar")
     public double processaSoma (double termo1, double termo2) {
-        return termo1 + termo2;
+        return Calculadora.somar(termo1, termo2);
     }
     
     @WebMethod(operationName = "subtrair")
     public double processaSubtracao (double termo1, double termo2) {
-        return termo1 - termo2;
+        return Calculadora.subtrair(termo1, termo2);
     }
     
     @WebMethod(operationName = "multiplicacao")
     public double processaMultiplicacao (double termo1, double termo2) {
-        return termo1 * termo2;
+        return Calculadora.multiplicar(termo1, termo2);
     }
     
     @WebMethod(operationName = "dividir")
     public double processaDivisao (double termo1, double termo2) {
-        if (termo2 > 0) {
-            return termo1 / termo2;
-        } else {
-            return 0;
-        }
+        return Calculadora.dividir(termo1, termo2);
     }
 }
